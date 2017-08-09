f358b0e9157812ba0954dbe090c0807852525b79
rern <rernrern@gmail.com>
2017-08-09 15:25:56 +0700

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -12,26 +12,35 @@
 
 # run ./ts.sh 
 
+url=https://s16.openstream.io/hls/qvsbe6etnpblgwsztq3kap62j7azw4e5mfbl5pcvv2gl472at67qop7z2qfa/seg-{1..200}-v1-a1.ts
 name='Flesh And Bone S01E08.ts'
 dir='/mnt/hdd/Movies/Flesh And Bone 1'
-url=https://s16.openstream.io/hls/qvsbe6etnpblgwsztq3kap62j7azw4e5mfbl5pcvv2gl472at67qop7z2qfa/seg-{1..200}-v1-a1.ts
+file="$dir/$name"
 
 time0=$( date +%s )
 
-[[ ! -e $dir ]] && mkdir $dir
+mkdir -p $dir
 
-[[ -e "$dir/$name" ]] && name+=.1
+yesno() { # $1 = header string; $2 = input or <enter> = ''
+	echo -e "$1"
+	echo -e '  \e[0;36m0\e[m No'
+	echo -e '  \e[0;36m1\e[m Yes'
+	echo
+	echo -e '\e[0;36m0\e[m / 1 ? '
+	read -n 1 $2
+	echo
+}
+if [[ -e "$file" ]]; then
+  yesno "\e[30m\e[43m i \e[0m Overwrite existing $file:" ans
+  [[ $ans != 1 ]] && exit
+  rm $file
+fi
 
-echo -e "\n$dir/$name\n"
-wget -q --show-progress --tries=10 --retry-connrefused --timeout=2 --wait=1 "$url" -O - >> "$dir/$name"
+echo -e "\n$file\n"
+wget -q --show-progress --tries=10 --retry-connrefused --timeout=2 --wait=1 "$url" -O - >> "$file"
 
 time1=$( date +%s )
 timediff=$(( $time1 - $time0 ))
 timemin=$(( $timediff / 60 ))
 timesec=$(( $timediff % 60 ))
 echo -e "\nDuration: $timemin min $timesec sec"
-
-#last=5
-#url="https://s8.openstream.io/hls/qvsbeqetnpblgwsztq3ka2o7lcdod2tnpvpbk4ks55ebys5g5bexnir7afxq/seg-{1..$last}-v1-a1.ts"
-# 'eval' expands $last inside '{...}'
-#eval wget -q --show-progress $url -O - >> "$dir/$name.ts"
