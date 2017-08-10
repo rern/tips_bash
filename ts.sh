d6478a10f7907e93d465a963834610e3eed797f1
rern <rernrern@gmail.com>
2017-08-10 20:59:41 +0700

    Update ts.sh

--- ts.sh
+++ ts.sh
@@ -8,16 +8,19 @@
 # forward to end to get the last 'N' for 'total'
 
 # this file
-# edit /-1-/-$i-/ in: url0=$( echo $url | sed "s/-1-/-$i-/" )
+# edit increment pattern /-1-/-$i-/ in: url=$( echo $url1 | sed "s/-1-/-$i-/" )
 
 # run ./ts.sh 
 
-name='name.ts'
+name='The Farthest.ts'
 dir=/mnt/hdd/Movies
-url=https://path/seg-1-v1-a1.ts
-total=291
+url1=https://s36.gtsznokiyn.site/hls/qvsbfgdmnxblgwsztrd2a7wllpqmmlgeg7ovili7hdfaoj2psgrrjqldzkca/seg-1-v1-a1.ts
+total=3
 
+li=$( printf "\e[36m%*s\e[0m\n" "${COLUMNS:-$(tput cols)}" '' | tr ' ' "=" )
+bar='\e[36m\e[46m . \e[0m'
 yesno() { # $1 = header string; $2 = input or <enter> = ''
+	echo
 	echo -e "\e[30m\e[43m i \e[0m $1"
 	echo -e '  \e[0;36m0\e[m No'
 	echo -e '  \e[0;36m1\e[m Yes'
@@ -36,12 +39,14 @@ time0=$( date +%s )
 
 mkdir -p $dir
 
-echo -e "\n$dir/\e[36m$name\e[0m\n"
+echo $li
+echo -e "$bar $dir/\e[36m$name\e[0m"
+echo $li
 
 for (( i = 1; i <= $total; i++ )); do
-	url0=$( echo $url | sed "s/-1-/-$i-/" )
+	url=$( echo $url1 | sed "s/-1-/-$i-/" )
 	echo $i/$total
-	wget -q --show-progress --tries=10 --retry-connrefused --timeout=2 --wait=1 $url0 -O - >> "$dir/$name"
+	wget -q --show-progress --tries=10 --retry-connrefused --timeout=2 --wait=1 $url -O - >> "$dir/$name"
 done
 
 time1=$( date +%s )
@@ -51,5 +56,6 @@ min=$(( $dif / 60 ))
 sec=$(( $dif % 60 ))
 (( ${#sec} == 1 )) && sec=0$sec
 
-echo -e "\n$dir/\e[36m$name\e[0m downloaded in ${min}:$sec\n"
-
+echo $li
+echo -e "$bar $dir/\e[36m$name\e[0m downloaded in ${min}:$sec"
+echo $li
