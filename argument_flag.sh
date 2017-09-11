61ec9ef8880f36c467a98b2cfc80619acdb2d70f
rern <rernrern@gmail.com>
2017-09-11 13:35:30 +0700

    Update argument_flag.sh

--- argument_flag.sh
+++ argument_flag.sh
@@ -6,14 +6,14 @@ yesno=0
 while :; do
 	case $1 in
 		-s) [[ $2 != 0 ]] && string=$2
-      shift;;                       # 1st shift
-		-y) yesno=1;;                   # no 1st 'shift' for option without value
+			shift;;                # 1st shift
+		-y) yesno=1;;                  # no 1st 'shift' for option without value
 		*) break
 	esac
-	# shift 1 out of argument array '$@'
-	# 1.option + 1.value - shift twice
-	# 1.option + 0.without value - shift once
 	shift
+	# shift 1 out of argument array '$#'
+	# 1 flag + 1  value - shift twice
+	# 1 flag + '' value - shift once
 done
 
 echo $string # 'np -s' = ''; '-s string' = string
