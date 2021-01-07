366f0ebd25070fe1c91e3d18c2210c6e366e315a
rern <rernrern@gmail.com>
2021-01-07 13:18:31 +0700

    Update githubemail.sh

--- githubemail.sh
+++ githubemail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# usage: bash<( wget USER )
+# usage: githubemail.sh USER
 
 wget -qO - https://api.github.com/users/$1/events/public \
     | jq -r .[0].payload.commits[0].author.email
