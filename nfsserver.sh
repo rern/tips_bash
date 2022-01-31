ad4c5ff50ac44cd9f4564cb17bb2fa41ed4e890e
rern <rernrern@gmail.com>
2022-01-31 15:20:26 +0700

    Update nfsserver.sh

--- nfsserver.sh
+++ nfsserver.sh
@@ -5,24 +5,23 @@ optbox=( --colors --no-shadow --no-collapse )
 share=$( dialog "${optbox[@]}" --output-fd 1 --nocancel --inputbox "
  Shared path to create:
 " 0 0 )
-rwx=$( dialog "${optbox[@]}" --output-fd 1 --nocancel --menu "
+rw=$( dialog "${optbox[@]}" --output-fd 1 --nocancel --menu "
  \Z1Permission\Z0:
 " 8 0 0 \
-1 rwx \
-2 rw- \
-3 r-- )
-if [[ $rwx == 1 ]]; then
-	permission=777
-elif [[ $rwx == 2 ]]; then
+1 read + write \
+2 read only )
+if [[ $rw == 1 ]]; then
 	permission=666
+	rw=rw
 else
-	permission=555
+	permission=444
+	rw=ro
 fi
 
 [[ ! -e /usr/bin/nfsconf ]] && pacman -Sy nfs-utils # debian: nfs-kernel-server
 
 mkdir -p $share
 chmod -R $permission $share
-echo "$share 192.168.1.0/24(rw,sync,no_subtree_check)" >> /etc/exports
+! grep -q $share /etc/exports && echo "$share 192.168.1.0/24($rw,sync,no_subtree_check)" >> /etc/exports
 
 systemctl restart nfs-server # debian: nfs-kernel-server
