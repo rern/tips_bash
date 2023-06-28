7f74572c2a70476710a277843a2a538aed8b9517
rern <rernrern@gmail.com>
2023-06-28 14:09:01 +0700

    Delete github_email.sh

--- github_email.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# usage: bash <( wget https://github.com/rern/tips_bash/raw/main/githubemail.sh -qO - ) USER
-
-wget -qO - https://api.github.com/users/$1/events/public \
-    | jq -r .[0].payload.commits[0].author.email
